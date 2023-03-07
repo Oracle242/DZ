74105400b27b09ac448052830c09f4ed5fa21e25
oracle242 <psxnva@gmail.com>
2023-03-07 23:19:49 +0700

    HW_array3.0

--- shadow.go
+++ shadow.go
@@ -64,7 +64,7 @@ func (cart cartoteka) osnov() cartoteka {
 	fmt.Println("Выбор фильма от 1 до 10. 11 вернуться в меню.")
 	doubleMenu := 0
 	fmt.Scanln(&doubleMenu)
-	if doubleMenu < 10 {
+	if doubleMenu <= 10 {
 		fmt.Println(cart.film[doubleMenu-1])
 	}
 	return cart
