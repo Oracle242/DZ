bc72b8a89e65ac22a30f8f20de7a2268213a6bab
oracle242 <psxnva@gmail.com>
2023-03-15 18:46:36 +0700

    ++

--- shadow.go
+++ shadow.go
@@ -2,57 +2,31 @@ package main
 
 import "fmt"
 
-type Numbers struct {
-	num1 int
-	num2 int
-}
-
-type NumbersInterface interface {
-	Sum() int
-	Multiplay() int
-	Division() float64
-	Substract() int
-}
-
-func (n Numbers) Sum() int {
-	return n.num1 + n.num2
-}
-func (n Numbers) Substract() int {
-	return n.num1 - n.num2
-}
-func (n Numbers) Multiplay() int {
-	return n.num1 * n.num2
-}
-func (n Numbers) Division() float64 {
-	if n.num2 == 0 {
-		fmt.Println("На ноль делить низя")
-	}
-	return float64(n.num1) / float64(n.num2)
-}
 func main() {
+	nameArr := [6]string{"vasya", "petya", "masha", "vasya", "vasya", "fedya"}
+	slov := map[string]int{}
 
-	numb1 := 0
-	numb2 := 0
-	sign := "0"
-	fmt.Println("Введи первое число")
-	fmt.Scanln(&numb1)
-	fmt.Println("Введи +-*/")
-	fmt.Scanln(&sign)
-	fmt.Println("Введи второе число")
-	fmt.Scanln(&numb2)
+	// var mass []string
+	// var mass1 []int
 
-	var i NumbersInterface
-	numbers := Numbers{numb1, numb2}
-	i = numbers
+	// for key, valye := range slov {
+	// 	mass = append(mass, key)
+	// 	mass1 = append(mass1, valye)
+	// }
 
-	switch sign {
-	case "+":
-		fmt.Printf("Ответ: %d\n", i.Sum())
-	case "-":
-		fmt.Printf("Ответ: %d\n", i.Substract())
-	case "*":
-		fmt.Printf("Ответ: %d\n", i.Multiplay())
-	case "/":
-		fmt.Printf("Ответ: %f\n", i.Division())
-	}
+	fmt.Println(nameArr)
+	// fmt.Println(mass)
+	// fmt.Println(mass1)
 }
+
+// 1.
+// ['vasya', 'petya', 'masha', 'vasya', 'vasya', 'fedya']
+// посчитать с помощью map сколько раз встречается каждое имя
+//
+
+// 2.
+// ['vasya', 'petya', 'masha', 'vasya', 'vasya', 'fedya']
+// вывести уникальные имена
+// отсортировать имена
+// разобраться с range(с разными вариациями)
+// Изучить раздел misc.
