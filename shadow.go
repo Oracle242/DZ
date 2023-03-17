7172b1e3ae485a9da4dd2ebbf8595f8b3eb96f56
oracle242 <psxnva@gmail.com>
2023-03-17 23:04:00 +0700

    Array,Slise,Map,Сортировка вставкой.

--- shadow.go
+++ shadow.go
@@ -4,32 +4,35 @@ import "fmt"
 
 func main() {
 	channels := [7]string{"NBC", "CBS", "ABC", "Fox", "CW", "Russia1", "TNT"}
-
+	channels2 := []string{}
 	tricolor := make(map[string]string)
 
 	for i := 0; i < len(channels); i++ {
-		for j := 0; j < len(channels)-1; j++ {
-			if channels[j] > channels[j+1] {
-				cloudChannels := channels[j+1]
-				channels[j+1] = channels[j]
-				channels[j] = cloudChannels
+		min := channels[0]
+		minIndex := 0
+		for j := 0; j < len(channels); j++ {
+			if channels[j] < min {
+				min = channels[j]
+				minIndex = j
 			}
 		}
+		channels[minIndex] = "ZZZZ"
+		channels2 = append(channels2, min)
 	}
 
-	channelSlEng := channels[:4]
+	channelSlEng := channels2[:4]
 	for _, meaning := range channelSlEng {
 		tricolor[meaning] = "Eng"
 	}
 
-	channeSlRus := channels[5:7]
+	channeSlRus := channels2[5:7]
 	for _, meaning := range channeSlRus {
 		tricolor[meaning] = "rus"
 	}
 
-	fmt.Println(channels)
-	fmt.Println(channelSlEng)
-	fmt.Println(channeSlRus)
+	// fmt.Println(channels2)
+	// fmt.Println(channelSlEng)
+	// fmt.Println(channeSlRus)
 	fmt.Println(tricolor)
 }
 
