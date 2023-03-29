652f2034465c2a918577256e3a21af9a0ee71d29
oracle242 <psxnva@gmail.com>
2023-03-29 21:33:19 +0700

    Создание, запись, прочтение файла.

--- shadow.go
+++ shadow.go
@@ -2,16 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
+	film := "Терминатор"
 	file, err := os.Create("Фильмотека.txt")
 	if err != nil {
 		fmt.Printf("Возникла ошибка создания библиотеки фильмов [%s]", err)
 		return
 	}
-	fmt.Printf("Файл с названием %s создан", file.Name())
+
+	stringlen, err := file.WriteString(film)
+	if err != nil {
+		fmt.Printf("Возникла ошибка записи в библиотеку фильмов [%s]", err)
+		return
+	}
+
+	openFile, err := os.Open(file.Name())
+	if err != nil {
+		fmt.Printf("Возникла ошибка прочтения библиотеки фильмов [%s]", err)
+		return
+	}
+
+	data := make([]byte, stringlen)
+
+	for {
+		_, err := openFile.Read(data)
+		if err != io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("Ошибка чтения из файла [%s]", err)
+			break
+		}
+	}
+	fmt.Printf(string(data))
 }
 
 // Изучить раздел misc.
