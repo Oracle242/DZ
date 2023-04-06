9b39cf9dfb0febd1c249311ec77f223de4f7028c
oracle242 <psxnva@gmail.com>
2023-04-06 20:48:50 +0700

    ЗаписьИЧтение

--- shadow.go
+++ shadow.go
@@ -1,45 +1,77 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	film := "Терминатор"
-	file, err := os.Create("Фильмотека.txt")
+	fale := []string{}
+	znak := " !"
+	strSlice := []string{}
+	intSlice := []int{}
+
+	pathFile := "C:/Users/PSXnv/Desktop/Task.txt"
+
+	file, err := os.Open(pathFile)
 	if err != nil {
-		fmt.Printf("Возникла ошибка создания библиотеки фильмов [%s]", err)
-		return
+		log.Fatalf("Ошибка открытия файла: %s", err)
 	}
 
-	stringlen, err := file.WriteString(film)
-	if err != nil {
-		fmt.Printf("Возникла ошибка записи в библиотеку фильмов [%s]", err)
-		return
+	fileScanner := bufio.NewScanner(file)
+
+	for fileScanner.Scan() {
+		fale = append(fale, fileScanner.Text())
 	}
 
-	openFile, err := os.Open(file.Name())
-	if err != nil {
-		fmt.Printf("Возникла ошибка прочтения библиотеки фильмов [%s]", err)
-		return
+	if err := fileScanner.Err(); err != nil {
+		log.Fatalf("Ошибка при чтении файла: %s", err)
 	}
+	file.Close()
 
-	data := make([]byte, stringlen)
-
-	for {
-		_, err := openFile.Read(data)
-		if err != io.EOF {
-			break
+	for i := 0; i < len(fale); i++ {
+		if fale[i] < "0" || fale[i] > "999" {
+			strSlice = append(strSlice, fale[i]+znak)
+		} else {
+			numbr, err := strconv.Atoi(fale[i])
+			if err != nil {
+				log.Fatal(err)
+			}
+			intSlice = append(intSlice, numbr*2)
 		}
+	}
 
+	createFile, err := os.Create("random.txt")
+	if err != nil {
+		fmt.Printf("Ошибка открытия файла %s", err)
+		return
+	}
+	defer createFile.Close()
+
+	writer := bufio.NewWriter(createFile)
+
+	for _, str := range strSlice {
+		_, err = writer.WriteString(str + "\n")
 		if err != nil {
-			fmt.Printf("Ошибка чтения из файла [%s]", err)
-			break
+			fmt.Printf("ошибка считывания данных %s", err)
+			return
 		}
 	}
-	fmt.Printf(string(data))
+	for _, num := range intSlice {
+		_, err = writer.WriteString(strconv.Itoa(num) + "\n")
+		if err != nil {
+			fmt.Printf("ошибка считывания данных %s", err)
+			return
+		}
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		fmt.Printf("Ошибка записи в буфер %s", err)
+	}
 }
 
 // Изучить раздел misc.
