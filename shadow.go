c5090a5db51faeb82eb30cb74e4af12a4185c314
oracle242 <psxnva@gmail.com>
2023-04-19 23:35:15 +0700

    search_max_min

--- shadow.go
+++ shadow.go
@@ -2,18 +2,31 @@ package main
 
 import "fmt"
 
-func bubbleSort(arr []int) {
-	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr)-1; j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-			}
+func findMinAndMax(arr []int) (int, int) {
+	var min, max int
+	if len(arr) == 1 {
+		return arr[0], arr[0]
+	}
+	if arr[0] > arr[1] {
+		max = arr[0]
+		min = arr[1]
+	} else {
+		max = arr[1]
+		min = arr[0]
+	}
+	for i := 2; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		} else if arr[i] < min {
+			min = arr[i]
 		}
 	}
+	return max, min
 }
 
 func main() {
 	arr := []int{77, 32, -5, 677, -99, 223, -34, 78, 92, 13}
-	bubbleSort(arr)
-	fmt.Println("Sorted array:", arr)
+	min, max := findMinAndMax(arr)
+	fmt.Println("Array:", arr)
+	fmt.Printf("Min: %d, Max: %d\n", max, min)
 }
