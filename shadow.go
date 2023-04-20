28639499df107fbe10a8300684151f3f9b154bc4
oracle242 <psxnva@gmail.com>
2023-04-20 18:16:38 +0700

    is_prime

--- shadow.go
+++ shadow.go
@@ -1,17 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func gcd(a, b int) int {
-	if b == 0 {
-		return a
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
 	}
-	return gcd(b, a%b)
+	sqrtN := int(math.Sqrt(float64(n)))
+	for i := 2; i < sqrtN; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-	a, b := 30, 42
-	fmt.Printf("GCD of %d and %d is %d\n", a, b, gcd(a, b))
+	n := 99901
+	if isPrime(n) {
+		fmt.Printf("%d is a prime number\n", n)
+	} else {
+		fmt.Printf("%d is not a prime number\n", n)
+	}
 
 }
 
