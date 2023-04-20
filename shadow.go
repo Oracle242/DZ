ce3d1bb37faf52ddf1a872a9e945507db7173b69
oracle242 <psxnva@gmail.com>
2023-04-20 18:02:14 +0700

    Max_del

--- shadow.go
+++ shadow.go
@@ -2,31 +2,19 @@ package main
 
 import "fmt"
 
-func findMinAndMax(arr []int) (int, int) {
-	var min, max int
-	if len(arr) == 1 {
-		return arr[0], arr[0]
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
 	}
-	if arr[0] > arr[1] {
-		max = arr[0]
-		min = arr[1]
-	} else {
-		max = arr[1]
-		min = arr[0]
-	}
-	for i := 2; i < len(arr); i++ {
-		if arr[i] > max {
-			max = arr[i]
-		} else if arr[i] < min {
-			min = arr[i]
-		}
-	}
-	return max, min
+	return gcd(b, a%b)
 }
 
 func main() {
-	arr := []int{77, 32, -5, 677, -99, 223, -34, 78, 92, 13}
-	min, max := findMinAndMax(arr)
-	fmt.Println("Array:", arr)
-	fmt.Printf("Min: %d, Max: %d\n", max, min)
+	a, b := 30, 42
+	fmt.Printf("GCD of %d and %d is %d\n", a, b, gcd(a, b))
+
 }
+
+/*поиск максимального делителя
+
+ */
