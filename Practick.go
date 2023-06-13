4e3f8320b0655e15e6543ca9f04b71cc3dbd1f1e
oracle242 <psxnva@gmail.com>
2023-06-13 19:16:28 +0700

    Palindrome

--- Practick.go
+++ Practick.go
@@ -2,22 +2,38 @@ package main
 
 import "fmt"
 
-func searchPrimeNumbers(numb int) int {
-	for i := numb - 1; i > 1; i-- {
-		if numb%i == 0 {
-			return 0
-		}
+func palindromeSearch(nubm string) {
+	essence := []string{}
+	reflection := []string{}
+	for i := 0; i <= len(string(nubm))-1; i++ {
+		essence = append(essence, string(nubm[i]))
 	}
-	return numb
+	for j := 1; j <= len(string(nubm)); j++ {
+		reflection = append(reflection, string(nubm[len(string(nubm))-j]))
+	}
+
+	comparisonEssAndRefl(essence, reflection)
 }
 
-func main() {
-	counter := 0
-	for i := 2; counter < 20; i++ {
-		elNu := searchPrimeNumbers(i)
-		if elNu != 0 {
-			fmt.Println(elNu)
-			counter++
+func comparisonEssAndRefl(ess, refl []string) {
+	for i := 0; i < len(ess)-1; i++ {
+		if ess[i] != refl[i] {
+			fmt.Printf("Ваше число %s не является палиндромом\n", ess)
+			return
+		} else {
+			fmt.Printf("Ваше число %s является палиндромом \n", ess)
+			return
 		}
 	}
+
+}
+func main() {
+	nubm := ""
+	fmt.Println("Укажите число для проверки на палиндром")
+	fmt.Scanln(&nubm)
+	if len(string(nubm)) == 1 || nubm <= string("1") {
+		fmt.Println("Палиндром является число которое больше 9 и не является отрицательным числом")
+		return
+	}
+	palindromeSearch(nubm)
 }
