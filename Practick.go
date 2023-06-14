784055d1eae325d5d4c6aad70480b11a35c8b879
oracle242 <psxnva@gmail.com>
2023-06-14 23:44:10 +0700

    archiver

--- Practick.go
+++ Practick.go
@@ -2,21 +2,30 @@ package main
 
 import "fmt"
 
-func palindromeSearch(num string) bool {
-	if string(num[0]) == "-" {
-		return false
-	}
-	for i := 1; i < len(num)+1; i++ {
-		if num[i-1] != num[len(num)-i] {
-			return false
+// сортировка на символы
+func sorting(sumols string) {
+	for i := 0; i < len(sumols)-1; i++ {
+		if sumols[i] != sumols[i+1] {
+			archive(sumols, string(sumols[i]))
 		}
 	}
-	return true
+	archive(sumols, string(sumols[len(sumols)-1]))
+	fmt.Printf("\n")
+}
+
+// подсчет повторений символов
+func archive(sumols, sumol string) {
+	r := 0
+	for i := 0; i < len(sumols); i++ {
+		if sumol == string(sumols[i]) {
+			r++
+		}
+	}
+	fmt.Printf("%v%v", sumol, r)
 }
 
 func main() {
-	numb := ""
-	fmt.Println("Укажите число для проверки на палиндром")
-	fmt.Scanln(&numb)
-	fmt.Println(palindromeSearch(numb))
+	sumols := "aaabbbbccccccr"
+	fmt.Println("Укажите строку для проверки на архива")
+	sorting(sumols)
 }
