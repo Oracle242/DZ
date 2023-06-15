b3391030496948cc9940d7d3cb4d53fe465f9d63
oracle242 <psxnva@gmail.com>
2023-06-15 19:38:10 +0700

    archiver 2.0

--- Practick.go
+++ Practick.go
@@ -4,28 +4,31 @@ import "fmt"
 
 // сортировка на символы
 func sorting(sumols string) {
+	enSt := 0
 	for i := 0; i < len(sumols)-1; i++ {
 		if sumols[i] != sumols[i+1] {
-			archive(sumols, string(sumols[i]))
+			enSt = i + 1
+			archive(sumols, string(sumols[i]), enSt)
 		}
 	}
-	archive(sumols, string(sumols[len(sumols)-1]))
+	archive(sumols[enSt:], string(sumols[len(sumols)-1]), len(sumols[enSt:]))
 	fmt.Printf("\n")
 }
 
 // подсчет повторений символов
-func archive(sumols, sumol string) {
+func archive(sumols, sumol string, enSt int) {
 	r := 0
-	for i := 0; i < len(sumols); i++ {
+	for i := 0; i < enSt; i++ {
 		if sumol == string(sumols[i]) {
 			r++
 		}
+
 	}
 	fmt.Printf("%v%v", sumol, r)
 }
 
 func main() {
-	sumols := "aaabbbbccccccr"
+	sumols := "aaabbbbccccccaaaa"
 	fmt.Println("Укажите строку для проверки на архива")
 	sorting(sumols)
 }
