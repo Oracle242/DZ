223947264e2f944763f5cbbc1e5a3f4ff9e95c65
oracle242 <psxnva@gmail.com>
2023-06-16 18:21:56 +0700

    archiver_remaster

--- Practick.go
+++ Practick.go
@@ -2,33 +2,24 @@ package main
 
 import "fmt"
 
-// сортировка на символы
+// Архивация
 func sorting(sumols string) {
-	enSt := 0
+	r := 1
 	for i := 0; i < len(sumols)-1; i++ {
-		if sumols[i] != sumols[i+1] {
-			enSt = i + 1
-			archive(sumols, string(sumols[i]), enSt)
+		if sumols[i] == sumols[i+1] {
+			r++
+		} else {
+			fmt.Printf("%v%v", string(sumols[i]), r)
+			r = 1
 		}
+
 	}
-	archive(sumols[enSt:], string(sumols[len(sumols)-1]), len(sumols[enSt:]))
+	fmt.Printf("%v%v", string(sumols[len(sumols)-1]), r)
 	fmt.Printf("\n")
 }
 
-// подсчет повторений символов
-func archive(sumols, sumol string, enSt int) {
-	r := 0
-	for i := 0; i < enSt; i++ {
-		if sumol == string(sumols[i]) {
-			r++
-		}
-
-	}
-	fmt.Printf("%v%v", sumol, r)
-}
-
 func main() {
-	sumols := "aaabbbbccccccaaaa"
-	fmt.Println("Укажите строку для проверки на архива")
+	sumols := "zaaabbzbbccccccaaaaa"
+	fmt.Println("Укажите строку для архивации")
 	sorting(sumols)
 }
