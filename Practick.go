e7957828e05871dc82274d9a1ef1808abfe9cd21
oracle242 <psxnva@gmail.com>
2023-06-19 19:22:38 +0700

    ОптимизацияЗадача№2

--- Practick.go
+++ Practick.go
@@ -2,24 +2,30 @@ package main
 
 import "fmt"
 
-// Архивация
-func sorting(sumols string) {
-	r := 1
-	for i := 0; i < len(sumols)-1; i++ {
-		if sumols[i] == sumols[i+1] {
-			r++
-		} else {
-			fmt.Printf("%v%v", string(sumols[i]), r)
-			r = 1
+func findMostOftenRepeatedOptimized(array []int) (mostOften int, err error) {
+	if len(array) == 0 {
+		return 0, fmt.Errorf("could not found repeated numbers in empty slice")
+	}
+
+	var maxIndex, maxCount = 0, 0
+	for i, number := range array {
+		currentCount := 0
+		for i, _ := range array {
+			if number == array[i] {
+				currentCount++
+			}
 		}
 
+		if currentCount > maxCount {
+			maxIndex = i
+			maxCount = currentCount
+		}
 	}
-	fmt.Printf("%v%v", string(sumols[len(sumols)-1]), r)
-	fmt.Printf("\n")
+
+	return array[maxIndex], nil
 }
 
 func main() {
-	sumols := "zaaabbzbbccccccaaaaa"
-	fmt.Println("Укажите строку для архивации")
-	sorting(sumols)
+	num := []int{1, 1, 2, 3, 6, 4, 3, 2, 3, 1, 3}
+	fmt.Println(findMostOftenRepeatedOptimized(num))
 }
