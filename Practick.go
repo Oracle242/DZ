bfe081e771889c4c4767df4a1885783a374333cc
oracle242 <psxnva@gmail.com>
2023-06-21 19:53:15 +0700

    Задача №4

--- Practick.go
+++ Practick.go
@@ -2,27 +2,19 @@ package main
 
 import "fmt"
 
-func findMostOftenRepeatedWithMap(array []int) (max int, err error) {
-	if len(array) == 0 {
-		return 0, fmt.Errorf("не удалось найти повторяющиеся числа в пустом срезе")
-	}
-	mapps := make(map[int]int)
-	max = 0
-	repetition := 0
-	for i := 0; i < len(array); i++ {
-		mapps[array[i]] += 1
-		if mapps[array[i]] > repetition {
-			repetition = mapps[array[i]]
-		}
-		if mapps[array[i]] == repetition {
-			max = array[i]
+func trimNegative(array []int) (max []int) {
 
+	for i := 0; i < len(array)-1; i++ {
+		if array[i] < 0 {
+			array = append(array[:i], array[i+1:]...)
 		}
+
 	}
-	return max, fmt.Errorf("Ошибки не обнаружены!")
+
+	return array
 }
 
 func main() {
-	num := []int{7, 7, 7, 1, 8, 2, 6, 6, 6, 8, 3, 8, 2, 8, 1, 9, 9, 9}
-	fmt.Println(findMostOftenRepeatedWithMap(num))
+	num := []int{8, 1, -3, 4}
+	fmt.Println(trimNegative(num))
 }
