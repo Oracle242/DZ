2a1a762d142c04d1d360a71fcc0bd0a879e582b5
oracle242 <psxnva@gmail.com>
2023-06-22 19:57:55 +0700

    Задача №5v2

--- Practick.go
+++ Practick.go
@@ -2,24 +2,29 @@ package main
 
 import "fmt"
 
-func trimLessAverage(array []int) []int {
-	total := 0
+func trimLessAverage(array []int) ([]int, error) {
+	array2 := []int{}
+	total, average := 0, 0
+
 	for _, numb := range array {
 		total += numb
 	}
 
-	array2 := []int{}
-	average := total / len(array)
+	if total > len(array) {
+		average = total / len(array)
+	} else {
+		return array2, fmt.Errorf("Среднее число не высчитать")
+	}
 
 	for i := 0; i < len(array); i++ {
 		if array[i] > average {
 			array2 = append(array2, array[i])
 		}
 	}
-	return array2
+	return array2, nil
 }
 
 func main() {
-	num := []int{1, 9, 1, 0, 9, 5, 0, 1}
+	num := []int{1, 2}
 	fmt.Println(trimLessAverage(num))
 }
