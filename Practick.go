df5552d3166d2fb9dc29688e5b220f902cb9f5c3
oracle242 <psxnva@gmail.com>
2023-06-22 19:36:55 +0700

    Задача №5

--- Practick.go
+++ Practick.go
@@ -2,19 +2,24 @@ package main
 
 import "fmt"
 
-func trimNegative(array []int) (max []int) {
-
-	for i := 0; i < len(array)-1; i++ {
-		if array[i] < 0 {
-			array = append(array[:i], array[i+1:]...)
-		}
-
+func trimLessAverage(array []int) []int {
+	total := 0
+	for _, numb := range array {
+		total += numb
 	}
 
-	return array
+	array2 := []int{}
+	average := total / len(array)
+
+	for i := 0; i < len(array); i++ {
+		if array[i] > average {
+			array2 = append(array2, array[i])
+		}
+	}
+	return array2
 }
 
 func main() {
-	num := []int{8, 1, -3, 4}
-	fmt.Println(trimNegative(num))
+	num := []int{1, 9, 1, 0, 9, 5, 0, 1}
+	fmt.Println(trimLessAverage(num))
 }
