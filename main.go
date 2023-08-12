795f5496f653e27b3007899af6ac95dfce717fe9
oracle242 <psxnva@gmail.com>
2023-08-12 17:12:11 +0700

    BankClient

--- main.go
+++ main.go
@@ -7,70 +7,77 @@ import (
 	"time"
 )
 
-type BankClient interface {
-	Deposit(amount int)
-	Withdrawal(amount int) error
-	Balance() int
+type BankClient struct {
+	balance int
 }
 
-// type Client struct {
-// 	balance int
-// }
-
-// func (c *Client) Balance() int {
-// 	return c.balance
-// }
-
-var mu = sync.Mutex{}
+var mu = sync.RWMutex{}
 
 func main() {
+	client := &BankClient{}
 	rand.Seed(time.Now().UnixNano())
-	var amount int
-	menu(amount)
-}
-
-func deposit(amount *int) error {
-	for {
-		time.Sleep(time.Second / 5)
-		mu.Lock()
-		result := *amount
-		result += rand.Intn(10) + 1
-		*amount = result
-		mu.Unlock()
-	}
-}
-
-func withdrawal(amount *int) error {
-	for {
-		time.Sleep(time.Second / 5)
-		mu.Lock()
-		result := *amount
-		result -= rand.Intn(5) + 1
-		*amount = result
-		fmt.Println(result, "tw")
-		mu.Unlock()
-	}
-
-}
-
-func menu(amount int) {
+	amount := 0
 	for i := 0; i < 10; i++ {
-		go deposit(&amount)
+		go func() {
+
+			for {
+				amount := rand.Intn(10) + 1
+				client.Deposit(&amount)
+			}
+		}()
 	}
 	for i := 0; i < 5; i++ {
-		go withdrawal(&amount)
+		go func() {
+			for {
+				amount := rand.Intn(5) + 1
+				err := client.Withdrawal(&amount)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}()
 	}
 	for {
 		command := ""
 		fmt.Scanln(&command)
 		switch command {
 		case "balance":
+			fmt.Println(client.Balance())
 		case "deposit":
+			fmt.Scanln(&amount)
+			client.Deposit(&amount)
+			fmt.Println(client.Balance())
 		case "withdrawal":
+			fmt.Scanln(&amount)
+			client.Withdrawal(&amount)
+			fmt.Println(client.Balance())
 		case "exit":
 			return
 		default:
 			fmt.Println("Unsupported command. You can use commands: balance, deposit, withdrawal, exit")
 		}
 	}
+}
+
+func (bc *BankClient) Deposit(amount *int) {
+	time.Sleep(time.Second / 5)
+	mu.RLock()
+	bc.balance += *amount
+	mu.RUnlock()
+}
+
+func (bc *BankClient) Withdrawal(amount *int) error {
+	time.Sleep(time.Second / 5)
+	mu.RLock()
+	if bc.balance < *amount {
+		return fmt.Errorf("not enough balance")
+	}
+	bc.balance -= *amount
+	mu.RUnlock()
+	return nil
+
+}
+
+func (bc *BankClient) Balance() int {
+	return bc.balance
 }
